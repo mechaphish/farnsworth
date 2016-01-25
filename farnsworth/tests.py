4517322a2be00aa3ddfa2ee901c85d61ab7b2fc7
Kevin Borgolte <kevin@borgolte.me>
2016-01-24 19:35:19 -0800

    add tests_put endpoint

--- farnsworth/tests.py
+++ farnsworth/tests.py
@@ -10,12 +10,12 @@ __version__ = "0.0.0"
 
 from . import app, postgres
 from flask import request
-from utils import jsonify, filter_query
+from utils import jsonify, filter_query, update_query
 
 
 @app.route("/tests")
 @jsonify
-def tests_status():
+def tests_get():
     """The ``/tests`` endpoint can be used to check if the API is reachable
     and if it is alive.
 
@@ -34,7 +34,8 @@ def tests_status():
 
     filterable_cols = ['id', 'ctn_id', 'job_id']
 
-    cursor.execute(*filter_query("""SELECT id, ctn_id, job_id, type, data
+    cursor.execute(*filter_query("""SELECT id, ctn_id, job_id, type,
+                                           encode(data, 'base64') as data
                                       FROM tests""",
                                  filterable_cols, request.args))
 
@@ -43,15 +44,39 @@ def tests_status():
 
 @app.route("/tests", methods=["POST"])
 @jsonify
-def tests_add():
+def tests_post():
     cursor = postgres.cursor(dictionary=False)
 
     tests = request.get_json()
     if isinstance(tests, dict):
         tests = [tests]
 
+    for test in tests:
+        # test['data'] = test['data'].decode('base64')
+        print(cursor.mogrify("""INSERT INTO tests (ctn_id, job_id, type, data)
+                          VALUES (%(ctn_id)s, %(job_id)s, %(type)s, %(data)s)""", test))
+
     cursor.executemany("""INSERT INTO tests (ctn_id, job_id, type, data)
                           VALUES (%(ctn_id)s, %(job_id)s, %(type)s, %(data)s)""",
                        tuple(tests))
 
     return {"status": "added"}  # FIXME should check if added
+
+
+@app.route("/tests", methods=["PUT"])
+@jsonify
+def tests_put():
+    cursor = postgres.cursor(dictionary=False)
+
+    tests = request.get_json()
+    if isinstance(tests, dict):
+        tests = [tests]
+
+    for test in tests:
+        where = {'id': test['id']}
+        updates = test
+        del updates['id']
+
+        cursor.execute(*update_query('tests', updates, where))
+
+    return {"status": "updated"}  # FIXME should check if added
