f9cfcccec4a152cf0f64ca619fb25eedaadd5ad2
Francesco Disperati <francesco@cs.ucsb.edu>
2016-01-26 18:34:39 -0800

    Set default value for LISTEN

--- farnsworth/config.py
+++ farnsworth/config.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 
 def set_config_from_env(app, path):
-    load_dotenv(path)
-    for k in os.environ:
-        app.config[k] = os.environ[k]
+    app.config.setdefault('LISTEN', '0.0.0.0')
+    if os.path.isfile(path):
+        load_dotenv(path)
+        for k in os.environ:
+            app.config[k] = os.environ[k]
