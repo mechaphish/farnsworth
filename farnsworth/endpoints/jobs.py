cb5d8ca607f5d13576e5a538fc188c427a1031ab
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-11 18:07:53 -0800

    Update job endpoint

--- farnsworth/endpoints/jobs.py
+++ farnsworth/endpoints/jobs.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 from .. import app, postgres
 from flask import request
 from datetime import datetime
-from utils import jsonify, filter_query
+from utils import jsonify, filter_query, update_query
 import psycopg2
 
 
@@ -29,12 +29,14 @@ def list_jobs():
 
     return cursor.fetchall()
 
+
 @app.route("/jobs/<int:job_id>")
 @jsonify
 def get_job(job_id):
     cursor = postgres.cursor()
     cursor.execute("""SELECT * FROM jobs WHERE id = %s""", [job_id])
     return cursor.fetchone()
+
 
 @app.route("/jobs", methods=['POST'])
 @jsonify
@@ -47,6 +49,26 @@ def create_job():
     RETURNING *""",
                    (d['worker'], d['limit_cpu'], d['limit_memory'],
                     d['cbn_id'], psycopg2.Binary(str(d['payload']))))
+
+    if cursor.rowcount == 0:
+        return {"errors": []}
+    else:
+        return cursor.fetchone()
+
+
+@app.route("/jobs/<int:job_id>", methods=["PUT"])
+@jsonify
+def update_job(job_id):
+    cursor = postgres.cursor()
+
+    job = request.get_json()
+    job.update({
+        'payload': psycopg2.Binary(str(job['payload'])),
+        'updated_at': datetime.now()
+    })
+    where = {'id': job_id}
+    cursor.execute(*update_query('jobs', job, where))
+    cursor.execute("""SELECT * FROM jobs WHERE id = %s""", [job_id])
 
     if cursor.rowcount == 0:
         return {"errors": []}
