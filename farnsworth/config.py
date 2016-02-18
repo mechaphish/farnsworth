e820dc9bc010d3e4465a78847de1da0b2c00439b
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-17 17:22:36 -0800

    Fix ENV variables set in Kubernetes

--- farnsworth/config.py
+++ farnsworth/config.py
@@ -5,5 +5,5 @@ def set_config_from_env(app, path):
     app.config.setdefault('LISTEN', '0.0.0.0')
     if os.path.isfile(path):
         load_dotenv(path)
-        for k in os.environ:
-            app.config[k] = os.environ[k]
+    for k in os.environ:
+        app.config[k] = os.environ[k]
