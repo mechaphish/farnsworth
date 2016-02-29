1c577f0dc0246c98859154d522e217c30ddbc717
Yan <yans@yancomm.net>
2016-02-28 19:34:41 -0800

    added some more endpoints for bitmaps

--- farnsworth/endpoints/bitmaps.py
+++ farnsworth/endpoints/bitmaps.py
@@ -6,10 +6,32 @@ __version__ = "0.0.0"
 
 from .. import app, postgres
 from flask import request
-from utils import jsonify, update_query
+from utils import jsonify, filter_query, update_query
 from datetime import datetime
 import psycopg2
 
+@app.route("/bitmaps")
+@jsonify
+def list_bitmaps():
+    cursor = postgres.cursor()
+
+    filterable_cols = ['id', 'cbn_id', 'created_at', 'updated_at']
+    query = """SELECT id, created_at, updated_at, cbn_id, blob FROM bitmaps"""
+    cursor.execute(*filter_query(query, filterable_cols, request.args))
+
+    return cursor.fetchall()
+
+
+@app.route("/bitmaps/<int:bitmap_id>")
+@jsonify
+def get_bitmap(bitmap_id):
+    cursor = postgres.cursor()
+    cursor.execute("""SELECT * FROM bitmaps WHERE id = %s""", [bitmap_id])
+    result = cursor.fetchone()
+    if result:
+        return result
+    else:
+        return {'errors': ['not found']}, 404
 
 @app.route("/bitmaps", methods=['POST'])
 @jsonify
