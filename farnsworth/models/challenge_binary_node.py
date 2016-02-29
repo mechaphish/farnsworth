01ff7534e0f3663ab24971be00dddc709c89f225
Yan <yans@yancomm.net>
2016-02-29 14:57:54 -0800

    add a path

--- farnsworth/models/challenge_binary_node.py
+++ farnsworth/models/challenge_binary_node.py
@@ -1,10 +1,16 @@
-from peewee import *
-
+from peewee import * #pylint:disable=wildcard-import,unused-wildcard-import
 from .base import BaseModel
+
+import os
 
 class ChallengeBinaryNode(BaseModel):
     parent = ForeignKeyField(db_column='parent_id', null=True, rel_model='self', to_field='id')
     blob = BlobField(null=True)
     name = CharField()
+    cs_id = CharField()
     root = ForeignKeyField(db_column='root_id', null=True, rel_model='self', to_field='id', related_name='challenge_binary_nodes_root_set')
     # parent_path = UnknownField(null=True)  # FIXMEx
+
+    @property
+    def path(self):
+        os.path.join(__file__, '../../../cbs', os.environ['CGC_EVENT'], self.cs_id, self.name)
