d1d59d918a32dbf9741db0d783aa48371bbccedd
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-29 15:36:02 -0800

    Tests setup

--- tests/test_challenge_binary_node.py
+++ tests/test_challenge_binary_node.py
@@ -1,14 +1,9 @@
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '../.env')
-load_dotenv(dotenv_path)
-
 from nose.tools import *
 
+from . import loadenv
 from farnsworth import *
 
-class TestAllFixme:
+class TestChallengeBinaryNode:
     def test_relationship(self):
         cbn = ChallengeBinaryNode.create(name = "test")
         job = Job.create(worker = "afl", cbn = cbn)
