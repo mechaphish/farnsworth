66b792fca62d5f08a529ef02cbef0431e591fb57
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-29 16:53:21 -0800

    CBN.undrilled_tests

--- farnsworth/models/challenge_binary_node.py
+++ farnsworth/models/challenge_binary_node.py
@@ -14,3 +14,8 @@ class ChallengeBinaryNode(BaseModel):
     @property
     def path(self):
         return os.path.join(os.path.dirname(__file__), '../../../cbs', os.environ['CGC_EVENT'], self.cs_id, self.name)
+
+    @property
+    def undrilled_tests(self):
+        from .test import Test
+        return self.tests.where(Test.drilled == False)
