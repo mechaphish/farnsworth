5d23b01359d19f35660eaae9d379e534fe66ffed
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-29 17:25:31 -0800

    Base.find() for querying by id

--- farnsworth/models/base.py
+++ farnsworth/models/base.py
@@ -11,3 +11,10 @@ class BaseModel(Model):
     class Meta:
         database = db
         db_table_func = table_name
+
+    @classmethod
+    def find(cls, id):
+        try:
+            return cls.get(cls.id == id)
+        except cls.DoesNotExist:
+            return None
