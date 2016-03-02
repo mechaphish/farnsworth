16b7b7ff81004378f004018d3514a6ee8fd35ef6
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-01 19:06:05 -0800

    Add custom EnumField type

---
+++ farnsworth/peewee_extensions.py
@@ -0,0 +1,12 @@
+from peewee import *
+
+class EnumField(Field):
+    db_field = "enum"
+
+    def coerce(self, value):
+        if value not in self.choices:
+            raise Exception("Invalid Enum Value `%s`", value)
+        return str(value)
+
+    def get_column_type(self):
+        return "enum"
