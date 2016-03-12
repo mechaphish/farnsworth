baa2351334422051ebcbf63aa0e302096c5685ba
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-12 12:46:21 -0800

    CBs env variable rename

--- farnsworth/models/challenge_binary_node.py
+++ farnsworth/models/challenge_binary_node.py
@@ -9,11 +9,11 @@ class ChallengeBinaryNode(BaseModel):
     blob = BlobField()
     name = CharField()
     cs_id = CharField()
-    # parent_path = UnknownField(null=True)  # FIXMEx
+    # parent_path = UnknownField(null=True)  # FIXME
 
     @property
     def path(self):
-        return os.path.join(os.path.dirname(__file__), '../../../cbs', os.environ['CGC_EVENT'], self.cs_id, self.name)
+        return os.path.join(os.environ['CGC_CBS_PATH'], self.cs_id, self.name)
 
     @property
     def undrilled_tests(self):
