d047c2f28d653548241e4ad2e91cdbb05b0868f5
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-14 11:13:14 -0700

    CGC_CBS_PATH not used anymore, binaries are pulled from the API

--- farnsworth/models/challenge_binary_node.py
+++ farnsworth/models/challenge_binary_node.py
@@ -13,9 +13,18 @@ class ChallengeBinaryNode(BaseModel):
 
     @property
     def path(self):
-        return os.path.join(os.environ['CGC_CBS_PATH'], self.cs_id, self.name)
+        filename = "{}-{}-{}".format(self.id, self.cs_id, self.name)
+        filepath = os.path.join(os.path.expanduser("~"), filename) # FIXME: afl doesn't like /tmp
+        if not os.path.isfile(filepath):
+            open(filepath, 'wb').write(self.blob)
+            os.chmod(filepath, 0o777)
+        return filepath
 
     @property
     def undrilled_tests(self):
         from .test import Test
         return self.tests.where(Test.drilled == False)
+
+    def __del__(self):
+        if os.path.isfile(self.path):
+            os.remove(self.path)
