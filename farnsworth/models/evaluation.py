3953b977175a82d682acd2316cd400616ed02fbb
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-29 11:33:49 -0700

    Evaluation.update_or_create needs team_id too

--- farnsworth/models/evaluation.py
+++ farnsworth/models/evaluation.py
@@ -1,14 +1,22 @@
 from peewee import *
 from playhouse.postgres_ext import JSONField
 import json
+from datetime import datetime
 
 from .base import BaseModel
 from .round import Round
 from .team import Team
-from .round_related_model import RoundRelatedModel
 
-class Evaluation(BaseModel, RoundRelatedModel):
+class Evaluation(BaseModel):
     round = ForeignKeyField(Round, related_name='feedbacks')
     team = ForeignKeyField(Team, related_name='feedbacks')
     ids = JSONField()
     cbs =  JSONField()
+
+    @classmethod
+    def update_or_create(cls, round_, team, **kwargs):
+        update = cls.update(updated_at=datetime.now(),
+                            **kwargs).where(cls.round == round_,
+                                            cls.team == team)
+        if update.execute() == 0:
+            cls.create(round=round_, team=team, **kwargs)
