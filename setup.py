5ec3b4649036b245c2390b8d0a5606c627c901ad
Francesco Disperati <francesco@cs.ucsb.edu>
2016-04-05 17:06:05 -0700

    Update requirements for PyPy installations

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from distutils.core import setup
+import platform
+
+deps = [
+    "peewee>=2.8.0",
+    # tests
+    "python-dotenv==0.3.0",
+    "nose>=1.3.7",
+    "nose-timer>=0.5.0",
+    "coverage>=4.0.3",
+]
+
+if platform.python_implementation() == "PyPy":
+    deps.append("psycopg2cffi")
+else:
+    deps.append("psycopg2>=2.6.1")
 
 setup(
     name='farnsworth',
     version='0.0.1',
     packages=['farnsworth'],
-    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
+    install_requires=deps,
     description='Knowledge base of the Shellphish CRS',
     url='https://git.seclab.cs.ucsb.edu/cgc/farnsworth',
 )
