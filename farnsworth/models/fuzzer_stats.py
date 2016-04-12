8fc7c356fc81502e0fd8e753903950318ac0395a
Nick Stephens <nick.d.stephens@gmail.com>
2016-04-12 16:34:19 -0700

    Rename fuzzer_stat relation to fuzzer_stat_collection

--- farnsworth/models/fuzzer_stats.py
+++ farnsworth/models/fuzzer_stats.py
@@ -4,7 +4,7 @@ from .base import BaseModel
 from .challenge_binary_node import ChallengeBinaryNode
 
 class FuzzerStat(BaseModel):
-    cbn = ForeignKeyField(db_column='cbn_id', rel_model=ChallengeBinaryNode, to_field='id', related_name='fuzzer_stat')
+    cbn = ForeignKeyField(db_column='cbn_id', rel_model=ChallengeBinaryNode, to_field='id', related_name='fuzzer_stats_collection')
     pending_favs = IntegerField(null=True)
     pending_total = IntegerField(null=True)
     paths_total = IntegerField(null=True)
