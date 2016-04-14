9edc3ed2559c88fa27db5240b3f4a5938a259328
Nick Stephens <nick.d.stephens@gmail.com>
2016-04-13 18:18:59 -0700

    Give Test model a method for unsynced test case

--- farnsworth/models/test.py
+++ farnsworth/models/test.py
@@ -9,3 +9,7 @@ class Test(BaseModel):
     cbn = ForeignKeyField(ChallengeBinaryNode, db_column='cbn_id', related_name='tests')
     job = ForeignKeyField(Job, db_column='job_id', to_field='id', related_name='tests')
     drilled = BooleanField()
+
+    @classmethod
+    def unsynced_testcases(cls, worker, prev_sync_time):
+        return cls.select().where(cls.job.worker == worker & cls.created_at > prev_sync_time)
