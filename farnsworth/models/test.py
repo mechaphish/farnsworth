367100c4375389f29d8bb88e0304df8ae0592a32
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-04-29 12:26:52 -0700

    Adding to xml method for Test

--- farnsworth/models/test.py
+++ farnsworth/models/test.py
@@ -1,8 +1,9 @@
 from peewee import * #pylint:disable=wildcard-import,unused-wildcard-import
-
 from .base import BaseModel
 from .challenge_binary_node import ChallengeBinaryNode
 from .job import Job
+from crscommon.actions import CQE_POV, Data, Write
+
 
 class Test(BaseModel):
     blob = BlobField()
@@ -13,3 +14,18 @@ class Test(BaseModel):
     @classmethod
     def unsynced_testcases(cls, worker, prev_sync_time):
         return cls.select().join(Job).where((Job.worker == worker) & (cls.created_at > prev_sync_time))
+
+    def to_cqe_pov_xml(self):
+        """
+            Method to convert job into to cqe xml format
+            :return Xml Containing test data in CQE POV format
+        """
+        pov_header = """<?xml version="1.0" standalone="no" ?>
+                        <!DOCTYPE pov SYSTEM "/usr/share/cgc-docs/replay.dtd">
+                    """
+        pov = CQE_POV(self.cbn.id, [ ])
+        # the Write
+        pov.actions.append(Write([ Data(self.blob) ]))
+
+        return pov_header + str(pov)
+
