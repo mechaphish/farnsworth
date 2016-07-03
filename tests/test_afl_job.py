c8f1254f3a3744c4e59f43d00c3aae71721d1b8e
Kevin Borgolte <cao@cao.vc>
2016-07-03 00:26:12 -0700

    Switch to more explicit comparison

--- tests/test_afl_job.py
+++ tests/test_afl_job.py
@@ -14,14 +14,16 @@ class TestAFLJob:
     def test_added_completed(self):
         cbn = ChallengeBinaryNode.create(name="foo", cs_id="foo")
         job = AFLJob(cbn=cbn)
-        assert_raises(AFLJob.DoesNotExist, AFLJob.get, cbn=cbn)
+        assert_raises(AFLJob.DoesNotExist, AFLJob.get, AFLJob.cbn == cbn)
 
         useless_job = RexJob(cbn=cbn)
-        assert_raises(AFLJob.DoesNotExist, AFLJob.get, cbn=cbn)
+        assert_raises(AFLJob.DoesNotExist, AFLJob.get, AFLJob.cbn == cbn)
 
         job.save()
-        job = AFLJob.get(cbn=cbn)
+        job = AFLJob.get(AFLJob.cbn == cbn)
 
         job.completed_at = datetime.now()
         job.save()
-        assert_raises(AFLJob.DoesNotExist, AFLJob.get, cbn=cbn, completed_at=None)
+        assert_raises(AFLJob.DoesNotExist, AFLJob.get,
+                      AFLJob.cbn == cbn,
+                      AFLJob.completed_at == None)
