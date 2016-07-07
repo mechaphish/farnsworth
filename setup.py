5d770d0f78ac1365482e3bacdeedcf6a4a1d3355
Kevin Borgolte <cao@cao.vc>
2016-07-07 15:22:42 -0700

    Export farnsworth.models as package

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 import platform
 
-deps = [
-    "peewee>=2.8.0",
-    # tests
-    "python-dotenv==0.3.0",
-    "nose>=1.3.7",
-    "nose-timer>=0.5.0",
-    "coverage>=4.0.3",
-]
+deps = ["peewee>=2.8.0",
+        # tests
+        "python-dotenv==0.3.0",
+        "nose>=1.3.7",
+        "nose-timer>=0.5.0",
+        "coverage>=4.0.3"]
 
 if platform.python_implementation() == "PyPy":
     deps.append("psycopg2cffi")
@@ -17,9 +18,9 @@ else:
 
 setup(
     name='farnsworth',
-    version='0.0.1',
-    packages=['farnsworth'],
+    version="0.0.1",
+    packages=["farnsworth", "farnsworth.models"],
     install_requires=deps,
-    description='Knowledge base of the Shellphish CRS',
-    url='https://git.seclab.cs.ucsb.edu/cgc/farnsworth',
+    description="Knowledge base of the Shellphish CRS",
+    url="https://git.seclab.cs.ucsb.edu/cgc/farnsworth",
 )
