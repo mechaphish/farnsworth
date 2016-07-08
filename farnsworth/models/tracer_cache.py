a46e360af0ac127b7f54930c1283edc93cb1738e
Kevin Borgolte <cao@cao.vc>
2016-07-08 03:05:10 -0700

    Add import for ForeignKeyField

--- farnsworth/models/tracer_cache.py
+++ farnsworth/models/tracer_cache.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from peewee import BlobField
+from peewee import BlobField, ForeignKeyField
 
 from .base import BaseModel
 from .challenge_binary_node import ChallengeBinaryNode
