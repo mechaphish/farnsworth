62bed29b28fa59eec0494bd4aca2c9a1953e1181
Kevin Borgolte <cao@cao.vc>
2016-07-07 21:34:01 -0700

    Add field create support for enums

--- farnsworth/peewee_extensions.py
+++ farnsworth/peewee_extensions.py
@@ -1,10 +1,22 @@
-"""Extend Peewee basic types."""
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
 
 from peewee import Field
+
+"""Extend Peewee basic types."""
+
 
 class EnumField(Field):
     """Define a EnumField type"""
     db_field = "enum"
+
+    def pre_field_create(self, model):
+        cursor = self.get_database().get_conn().cursor()
+        choices_str = ", ".join(itertools.repeat("'%s'", len(self.choices))
+        cursor.execute("CREATE TYPE %s AS ENUM ({});".format(choices_str),
+                       (self.db_field, ) + tuple(self.choices))
 
     def coerce(self, value):
         if value not in self.choices:
