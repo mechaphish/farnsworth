033f38934a912a508444ede3f45a2019c9849927
Kevin Borgolte <cao@cao.vc>
2016-07-09 02:55:17 -0700

    Update Round tests

--- tests/test_round.py
+++ tests/test_round.py
@@ -3,12 +3,15 @@
 
 from __future__ import absolute_import, unicode_literals
 
-import datetime
+from datetime import datetime, timedelta
 
 from nose.tools import *
 
 from . import setup_each, teardown_each
 from farnsworth.models import Round
+
+NOW = datetime.now()
+
 
 class TestRound:
     def setup(self):
@@ -19,23 +22,20 @@ class TestRound:
 
     def test_current_round(self):
         assert_equals(Round.current_round(), None)
-
-        now = datetime.datetime.now()
-        Round.create(num=0, ends_at=(now + datetime.timedelta(seconds=15)))
-        Round.create(num=1, ends_at=(now + datetime.timedelta(seconds=25)))
-        Round.create(num=2, ends_at=(now + datetime.timedelta(seconds=35)))
+        Round.create(num=0, ends_at=NOW + timedelta(seconds=15))
+        Round.create(num=1, ends_at=NOW + timedelta(seconds=25))
+        Round.create(num=2, ends_at=NOW + timedelta(seconds=35))
         assert_equals(Round.current_round().num, 2)
 
     def test_at_timestamp(self):
-        now = datetime.datetime.now()
-        round0 = Round.create(num=0, created_at=(now + datetime.timedelta(seconds=10)),
-                              ends_at=(now + datetime.timedelta(seconds=15)))
-        round1 = Round.create(num=1, created_at=(now + datetime.timedelta(seconds=20)),
-                              ends_at=(now + datetime.timedelta(seconds=25)))
-        round2 = Round.create(num=2, created_at=(now + datetime.timedelta(seconds=30)),
-                              ends_at=(now + datetime.timedelta(seconds=35)))
+        round0 = Round.create(num=0, created_at=(NOW + timedelta(seconds=10)),
+                              ends_at=NOW + timedelta(seconds=15))
+        round1 = Round.create(num=1, created_at=(NOW + timedelta(seconds=20)),
+                              ends_at=NOW + timedelta(seconds=25))
+        round2 = Round.create(num=2, created_at=(NOW + timedelta(seconds=30)),
+                              ends_at=NOW + timedelta(seconds=35))
 
-        assert_is_none(Round.at_timestamp(now + datetime.timedelta(seconds=5)))
-        assert_equals(Round.at_timestamp(now + datetime.timedelta(seconds=15)).num, 0)
-        assert_equals(Round.at_timestamp(now + datetime.timedelta(seconds=25)).num, 1)
-        assert_equals(Round.at_timestamp(now + datetime.timedelta(seconds=35)).num, 2)
+        assert_is_none(Round.at_timestamp(NOW + timedelta(seconds=5)))
+        assert_equals(Round.at_timestamp(NOW + timedelta(seconds=15)).num, 0)
+        assert_equals(Round.at_timestamp(NOW + timedelta(seconds=25)).num, 1)
+        assert_equals(Round.at_timestamp(NOW + timedelta(seconds=35)).num, 2)
