3f49aaad9d42ab9f946b6a1d444f802ea3288a87
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-11 17:31:09 -0700

    Don't use ManyToMany for fieldings

--- farnsworth/models/ids_rule_fielding.py
+++ farnsworth/models/ids_rule_fielding.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import os
 
 from peewee import ForeignKeyField
-from playhouse.fields import ManyToManyField
 
 from .base import BaseModel
 from .ids_rule import IDSRule
@@ -21,19 +20,7 @@ class IDSRuleFielding(BaseModel):
     """IDSRuleFielding model"""
 
     ids_rule = ForeignKeyField(IDSRule, related_name='fieldings')
-    team = ManyToManyField(Team, related_name='ids_fieldings')
+    team = ForeignKeyField(Team, related_name='ids_fieldings')
     submission_round = ForeignKeyField(Round, related_name='ids_fieldings')
     available_round = ForeignKeyField(Round, related_name='ids_fieldings', null=True)
     fielded_round = ForeignKeyField(Round, related_name='ids_fieldings', null=True)
-
-    @classmethod
-    def create(cls, *args, **kwargs):
-        # Converting team to a list because its is a ManyToManyField
-        if 'team' in kwargs:
-            if isinstance(kwargs['team'], Team):
-                kwargs['team'] = [kwargs['team']]
-            team = kwargs.pop('team')
-
-        obj = super(cls, cls).create(*args, **kwargs)
-        obj.team = team
-        return obj
