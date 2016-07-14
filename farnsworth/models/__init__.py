6c2a21b6e660e9240d4f5c9b7c5c05307c077664
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-13 23:16:07 -0700

    Remove duplicates and sort imports alphabetically

--- farnsworth/models/__init__.py
+++ farnsworth/models/__init__.py
@@ -15,13 +15,11 @@ from .challenge_set import ChallengeSet
 from .challenge_set_fielding import ChallengeSetFielding
 from .crash import Crash
 from .evaluation import Evaluation
-from .exploit_fielding import ExploitFielding
 from .exploit import Exploit
 from .exploit_fielding import ExploitFielding
 from .feedback import Feedback
 from .function_identity import FunctionIdentity
 from .fuzzer_stats import FuzzerStat
-from .ids_rule_fielding import IDSRuleFielding
 from .ids_rule import IDSRule
 from .ids_rule_fielding import IDSRuleFielding
 from .job import (
@@ -50,7 +48,6 @@ from .job import (
     PollCreatorJob,
     PovTesterJob
 )
-
 from .patch_score import PatchScore
 from .pcap import Pcap
 from .pov_test_result import PovTestResult
@@ -60,8 +57,8 @@ from .rop_cache import RopCache
 from .round import Round
 from .score import Score
 from .team import Team
-from .tester_result import TesterResult
 from .test import Test
+from .tester_result import TesterResult
 from .tracer_cache import TracerCache
 from .valid_polls import ValidPoll
 
