74c5ddcee54993ee158d4de5e1946cd1866a1970
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-14 20:42:34 -0700

    Remove unused (and incorrect) properties from ChallengeBinaryNode

--- farnsworth/models/challenge_binary_node.py
+++ farnsworth/models/challenge_binary_node.py
@@ -63,18 +63,6 @@ class ChallengeBinaryNode(BaseModel):
         return prefixed_path
 
     @property
-    def undrilled_tests(self):
-        """Return all undrilled test cases."""
-        from .test import Test
-        return self.tests.where(Test.drilled == False)
-
-    @property
-    def not_colorguard_traced(self):
-        """Return all undrilled test cases."""
-        from .test import Test
-        return self.tests.where(Test.colorguard_traced == False)
-
-    @property
     def unsubmitted_patches(self):
         """All unsubmitted patches."""
         from .challenge_set_fielding import ChallengeSetFielding
