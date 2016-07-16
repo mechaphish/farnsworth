b27944107856dfc14e7d8d504199cdafa0cb8072
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-15 23:05:40 -0700

    Add sha256sum to IDSRule

--- farnsworth/models/ids_rule.py
+++ farnsworth/models/ids_rule.py
@@ -4,8 +4,9 @@
 from __future__ import absolute_import, unicode_literals
 
 from datetime import datetime
+import hashlib
 
-from peewee import ForeignKeyField, TextField
+from peewee import FixedCharField, ForeignKeyField, TextField
 
 from .base import BaseModel
 from .challenge_set import ChallengeSet
@@ -17,6 +18,7 @@ class IDSRule(BaseModel):
     """IDSRule model"""
     cs = ForeignKeyField(ChallengeSet, related_name='ids_rules')
     rules = TextField()
+    sha256 = FixedCharField(max_length=64) # this index shit doesn't work, we create it manually
 
     def submit(self):
         """Save submission at current round"""
@@ -25,3 +27,8 @@ class IDSRule(BaseModel):
         from .team import Team
         irf = IDSRuleFielding.create(ids_rule=self, submission_round=Round.current_round(),
                                      team=Team.get_our())
+
+    def save(self, **kwargs):
+        if self.sha256 is None:
+            self.sha256 = hashlib.sha256(self.rules).hexdigest()
+        return super(IDSRule, self).save(**kwargs)
--- tests/test_ids_rule.py
+++ tests/test_ids_rule.py
@@ -26,10 +26,23 @@ class TestIDSRule:
         Round.create(num=0, ends_at=NOW + timedelta(seconds=30))
         Team.create(name=Team.OUR_NAME)
         cs = ChallengeSet.create(name="foo")
-        ids = IDSRule.create(cs=cs, rules="aaa")
+        ids = IDSRule.create(cs=cs, rules="aaa", sha256="bbb")
 
         assert_equals(len(ids.fieldings), 0)
         ids.submit()
         assert_equals(len(ids.fieldings), 1)
         assert_equals(ids.fieldings.get().team, Team.get_our())
         assert_equals(ids.fieldings.get().submission_round, Round.current_round())
+
+    def test_generate_hash_on_create_and_save_if_missing(self):
+        cs = ChallengeSet.create(name="foo")
+
+        ids_create = IDSRule.create(cs=cs, rules="aaa")
+        assert_equals(ids_create.sha256, "9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0")
+
+        ids_save = IDSRule(cs=cs, rules="bbb")
+        ids_save.save()
+        assert_equals(ids_save.sha256, "3e744b9dc39389baf0c5a0660589b8402f3dbb49b89b3e75f2c9355852a3c677")
+
+        ids_set = IDSRule.create(cs=cs, rules="ccc", sha256="ddd")
+        assert_equals(ids_set.sha256, "ddd")
